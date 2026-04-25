c4dcdd19a5dbdb59b16c459f9725fdb07b0a41cd
lamarsal88 <l.alsalamah1@lancaster.ac.uk>
2026-04-25 13:35:17 +0100

    Add files via upload

---
+++ SnowProblem.java
@@ -0,0 +1,7 @@
+public class SnowProblem{
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+        System.out.println("second line");
+        System.out.println("third line");
+    }
+}
