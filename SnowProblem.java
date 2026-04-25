a7ddfeec9c0897212155622b14b76ae204b845db
“lamarsal88” <lamarsal2019@gmail.com>
2026-04-25 14:31:22 +0100

    fifth

--- SnowProblem.java
+++ SnowProblem.java
@@ -1,3 +1,11 @@
+/**
+this java code is the snow problem
+@param xxxx
+@author lamar alsalamah
+@return
+*/
+
+import javax.swing.*;
 public class SnowProblem{
     public static void main(String[] args) {
         System.out.println("This will be printed");
