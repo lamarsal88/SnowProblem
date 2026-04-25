92a517492b7e2742ca7bf88f5559250eb4ad8d86
“lamarsal88” <lamarsal2019@gmail.com>
2026-04-25 14:19:11 +0100

    fourth

--- SnowProblem.java
+++ SnowProblem.java
@@ -2,6 +2,6 @@ public class SnowProblem{
     public static void main(String[] args) {
         System.out.println("This will be printed");
         System.out.println("second line");
-        System.out.println("third line");
+    System.out.println("third line");
     }
 }
