8bf4c17e53b830d6a2c725b42b6279f6b9429089
“lamarsal88” <lamarsal2019@gmail.com>
2026-04-28 19:21:19 +0100

    one

--- SnowProblem.java
+++ SnowProblem.java
@@ -1,11 +1,23 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-// Import GUI components in Swing class
-// import JPanel class;
-
-/**this java code is the snow problem
-a frame by the name of SnowProblemFrame is created for the GUI window
+/**
+ * This game is called the snow problem. The board has 20 locations with size
+ * of 5 by 4 board. There are two trees on the board. The location of the
+ * trees can be changed by the gamer. On the board, there are two
+ * snow balls: one small snowball and one large snowball. There is
+ * also a head of the snowman. The balls can move horizontally or
+ * vertically on the board. If the snawball does not hit a tree of another ball
+ * or the snowman head, it falls off the board and the game is over. The snowman
+ * head can move vertically or horizontally on the board one step
+ * at a time. The trees are positioned at the beginning of the game and
+ * should not be moved during the game. The goal of the game is to rebuild
+ * the snowman by stacking the small snowball on the large snowball and stack
+ * the head of the snowman head on the small snowball.
+ *when one cell holds the full stack: large -> small -> head.
+ *A "You won!" dialog appears on victory.
+*Controls: click to place / select. Arrow keys (or buttons) to slide. R = reset.
 @param xxxx
 @author lamar alsalamah
 @return
@@ -43,15 +55,15 @@ SnowProblemFrame.setContentPane(SnowProblemPanel); // Use panel on Window
     SnowProblemDescription.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));// Set the borders to 1.
     SnowProblemDescription.setBackground(Color.RED); // Set the background color of the label to green
     SnowProblemFrame.add(SnowProblemDescription,BorderLayout.NORTH);/*Position of the label is set to the top and center of the frame.
-    /Print the game goal on the label*/
+    Print the game goal on the label*/
     SnowProblemDescription.setText("Can you help the snowman? Build the snowman by guiding snowballs around obstacles until they are perfectly positioned to stack");
 
 JButton b = new JButton("Press!"); 
 SnowProblemPanel.add(b); 
 SnowProblemFrame.setContentPane(SnowProblemPanel); 
+    b.setPreferredSize(new Dimension(120, 40));
+    SnowProblemPanel.add(b, BorderLayout.SOUTH);
 
-
-
-
+   
     }
 }
